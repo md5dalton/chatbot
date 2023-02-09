32c69e8b088dcd1e1e3600e9950c6a1ba95abfc9
md5dalton <md5daltonggs@gmail.com>
2023-02-09 20:23:42 +0200

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -1,13 +1,5 @@
-import { PlusIcon } from "@heroicons/react/24/outline"
-import UList from "./UList"
-
 export default () => (
-    <div className="p-2 h-screen bg-[#202123] md:min-w-[20rem]">
-        <button className="chat-row border border-gray-700">
-            <PlusIcon className="h-4 w-4" />
-            <p>New Chat</p>
-        </button>
-        {/* model selector */}
-        {/* chat list */}
+    <div>
+        login
     </div>
 )
