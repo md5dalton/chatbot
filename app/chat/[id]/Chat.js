a8ab72873e9e6bab0ab831989a2effe6c57d998f
md5dalton <md5daltonggs@gmail.com>
2023-02-09 20:54:53 +0200

    Update Chat.js

--- app/chat/[id]/Chat.js
+++ app/chat/[id]/Chat.js
@@ -1,3 +1,30 @@
+"use client"
+
+import UList from "@/components/UList"
+import Message from "./Message"
+
+const messages = [
+    {
+        user: {
+            name: "majara",
+            avatar: "/vercel.svg"
+        },
+        text: "who are you"
+    },
+    {
+        user: {
+            name: "chatgpt",
+            avatar: "/next.svg"
+        },
+        text: "I am your conscience"
+    }
+]
+
 export default () => (
-    <div className="grow">chat</div>
+    <div className="grow">
+        <UList
+            items={messages}
+            itemHandler={msg => <Message {...msg} botMessage={msg.user.name != "majara"} />}
+        />
+    </div>
 )
