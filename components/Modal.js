3ea755add5bda51cdd605e133bc06db61e86fb54
md5dalton <md5daltonggs@gmail.com>
2023-02-11 11:22:24 +0200

    Update Modal.js

--- components/Modal.js
+++ components/Modal.js
@@ -1,7 +1,7 @@
 export default ({ children, isOpen }) => (
     <div 
         className={`fixed top-0 left-0 z-20
-        w-full bg-black/50 transition-all duration-200 ease-in-out
+        w-full h-full bg-black/50 transition-all duration-200 ease-in-out
         ${isOpen ? "" : "translate-x-[-100vw]"}
         md:translate-x-0 md:w-auto`}>
         {children}
