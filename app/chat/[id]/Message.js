85ddc90e74373d58d807dd9d0c57d809d2d4c634
md5dalton <md5daltonggs@gmail.com>
2023-02-13 16:32:33 +0200

    Update Message.js

--- app/chat/[id]/Message.js
+++ app/chat/[id]/Message.js
@@ -1,30 +1,34 @@
 import { HandThumbDownIcon, HandThumbUpIcon, PencilIcon } from "@heroicons/react/24/outline"
-import Image from "next/image"
 
-export default ({ user: { avatar }, text, botMessage }) => (
-    <div className={`${botMessage ? " bg-[#434654]" : ""}`}>
-        <div className="flex gap-x-4 p-4 mx-auto max-w-3xl">
-            <Image
-                className="h-8 w-8"
-                height={32}
-                width={32}
-                src={avatar}
-            />
-            <div className="grow space-y-3">
-                <p className="whitespace-pre-wrap leading-7">{text}</p>
-                <div className={`flex items-center${botMessage ? "" : " justify-end"}`}>
-                    {
-                        botMessage ? (
-                            <>
-                                <button className="p-2"><HandThumbUpIcon className="w-5" /></button>
-                                <button className="p-2"><HandThumbDownIcon className="w-5" /></button>
-                            </>
-                        ) : (
-                            <button className="p-2"><PencilIcon className="w-5" /></button>
-                        ) 
-                    }
+export default ({ user, message }) => {
+    // console.log(message)
+    const botMessage = message.user === "chatgpt"
+
+    return (
+        <div className={`${!botMessage ? " bg-[#434654]" : ""}`}>
+            <div className="flex gap-x-4 p-4 mx-auto max-w-3xl">
+                <img
+                    className="h-8 w-8"
+                    height={32}
+                    width={32}
+                    src={botMessage ? "/chat.png" : user.image}
+                />
+                <div className="grow space-y-3">
+                    <p className="whitespace-pre-wrap leading-7">{message.text}</p>
+                    <div className={`flex items-center${botMessage ? "" : " justify-end"}`}>
+                        {
+                            botMessage ? (
+                                <>
+                                    <button className="p-2"><HandThumbUpIcon className="w-5" /></button>
+                                    <button className="p-2"><HandThumbDownIcon className="w-5" /></button>
+                                </>
+                            ) : (
+                                <button className="p-2"><PencilIcon className="w-5" /></button>
+                            ) 
+                        }
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-)
+    )
+}
