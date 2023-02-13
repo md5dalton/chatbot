a21f477d8b55fc4d61cc06cdf623ccce56a990b6
md5dalton <md5daltonggs@gmail.com>
2023-02-13 08:40:23 +0200

    Create queryAPI.js

---
+++ utils/queryAPI.js
@@ -0,0 +1,17 @@
+import chatgpt from "./chatgpt"
+
+export default async (prompt, chatId, model) => {
+    const res = await chatgpt.createCompletion({
+        model,
+        prompt,
+        temperature: 0.0,
+        top_p: 1,
+        max_tokens: 1000,
+        frequency_penalty: 0,
+        presence_penalty: 0
+    })
+    .then(({ data: { choices } }) => choices.shift().text)
+    .catch(({ message }) => `ChatGPT no answer: ${message}`)
+
+    return res
+}
