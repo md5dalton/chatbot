1597703404d782541c9a10aafcfb7939cc24cad7
md5dalton <md5daltonggs@gmail.com>
2023-02-13 08:40:10 +0200

    Update ask.js

--- pages/api/ask.js
+++ pages/api/ask.js
@@ -1,6 +1,11 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 export default function handler(req, res) {
-    res.status(200).json({ name: 'John Doe' })
-  }
+
+    const { text, chatId, model, session } = req.body
+
+    if (!text) return res.status(400).json({answer: "please enter some text" })
+    if (!chatId) return res.status(400).json({answer: "dev: chatId is not available" })
+    
+    // const message
+    res.status(200).json({ answer: 'John Doe' })
+}
   
